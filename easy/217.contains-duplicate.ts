d18eb2df91eba51ad1b74fa41d41d3c94d8666d5
Vivek <vivekmolkar@gmail.com>
2026-02-19 05:58:23 +0000

    contains-duplicate

---
+++ easy/217.contains-duplicate.ts
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=217 lang=typescript
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+    let set = new Set(nums);
+
+    return set.size !== nums.length;
+};
+// @lc code=end
+
